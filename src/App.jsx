5d48dc0fb8f9cd83f850236a6d2797660394e117
gismael🎄 <63933317+gismael-gzn@users.noreply.github.com>
2024-12-03 23:01:21 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className='flex flex-col'>
+      
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./src/*.jsx", "./src/pages/*.jsx", "./src/components/*.jsx"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
